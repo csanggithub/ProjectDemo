ed7597ba48c7e51ffcee711c6e6b94c479b1c0fc
子烟 杜 <907500395@qq.com>
2018-08-05 21:30:01 +0800

    update user login check

---
+++ ProjectDemo.Web.Controllers/Controllers/User/UserController.cs
@@ -0,0 +1,56 @@
+﻿using ProjectDemo.Web.Controllers.Controllers.User.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ProjectDemo.Web.Controllers.Controllers.User
+{
+    public class UserController: Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var login = new LoginVM { UserName = "Admin", Password = "123qwe" };
+                //ModelState.AddModelError("UserName", "用户名不存在");
+                if (loginVM.UserName == login.UserName && loginVM.Password == login.Password)
+                {
+
+                    //FormsAuthenticationTicket 
+                    FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(
+                        1,
+                        login.UserName,
+                        DateTime.Now,
+                        DateTime.Now.AddMinutes(20),
+                        false,
+                        "Admin"
+                        );
+                    var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, FormsAuthentication.Encrypt(ticket));
+                    cookie.HttpOnly = true;
+                    HttpContext.Response.Cookies.Add(cookie);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+
+        public string SingOut()
+        {
+            FormsAuthentication.SignOut();
+            return "退出";
+        }
+    }
+}
---
+++ ProjectDemo.Web.Controllers/Filter/AuthenticationAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace ProjectDemo.Web.Controllers.Filter
+{
+    public class AuthenticationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string role = string.Empty;
+            var cookie = filterContext.HttpContext.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (cookie != null)
+            {
+                var ticket = FormsAuthentication.Decrypt(cookie.Value);
+                role = ticket.UserData;
+                if (role.Length == 0 || role == "Admin")
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { Controller = "UserLogin", action = "Login" }));
+                }
+            }
+            else
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { Controller = "UserLogin", action = "Login" }));
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
